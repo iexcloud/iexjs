82b564ad7c180cd9a64e4309a66827f209737cf9
wsmith-iex <will.smith@iexcloud.io>
2022-06-16 06:22:15 +0000

    tests

---
+++ test/platform.test.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+/* ***************************************************************************
+ *
+ * Copyright (c) 2021, the iexjs authors.
+ *
+ * This file is part of the iexjs library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+/* eslint-disable no-undef */
+
+const fetch = require("cross-fetch");
+const { Client, overrideUrl } = require("../src/js");
+
+beforeAll(() => {
+  global.fetch = fetch;
+});
+
+console.log(process.argv);
+console.log(IEX_TOKEN);
+
+// set timeout to 30s for long calls
+jest.retryTimes(2);
+jest.setTimeout(50000);
+
+afterEach(async () => {
+  await new Promise((r) => setTimeout(r, 500));
+});
+
+describe("Dataset schemas", () => {
+  test("all schemas", async () => {
+    const client = new Client({ api_token: TOKEN});
+    const res = await client.platform.data({});
+    expect(res).toBeDefined();
+  });
+
+  test("workspace schemas", async () => {
+    const client = new Client({ api_token: TOKEN});
+    const res = await client.platform.data({workspace: WORKSPACE_1});
+    expect(res).toBeDefined();
+  });
+
+  test("workspace schemas", async () => {
+    const client = new Client({ api_token: TOKEN});
+    const res = await client.platform.data({workspace: WORKSPACE_1, id: DATASET_1});
+    expect(res).toBeDefined();
+  });
+
+  test("core schemas", async () => {
+    const client = new Client({ api_token: TOKEN});
+    const res = await client.platform.data({workspace: WORKSPACE_2});
+    expect(res).toBeDefined();
+  });
+});
